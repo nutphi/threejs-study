f62d51b80b790e8ec824a5cce582d2f2e7057885
Nuttakit <nuttakit@heyday.ai>
2022-02-17 22:36:53 +0700

    update for test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
 import { AfterViewInit, Component, OnInit } from '@angular/core';
+import { fromEvent, Subject, takeUntil } from 'rxjs';
 import * as THREE from 'three';
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
+import gsap from 'gsap';
 
 @Component({
   selector: 'app-root',
@@ -7,13 +10,15 @@ import * as THREE from 'three';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent implements AfterViewInit, OnInit {
+  readonly COUNT_VDO_RENDER_IN_SEC = 10;
   scene = new THREE.Scene();
-  sizes = {width: 800, height: 600};
-  mesh = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'red'}));
-  mesh2 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'blue'}));
-  mesh3 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'green'}));
-  mesh4 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'yellow'}));
-  group = new THREE.Group();
+  sizes = {width: window.innerWidth, height: window.innerHeight};
+  mesh = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'red', wireframe: true}));
+  mesh2 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'blue', wireframe: true}));
+  mesh3 = new THREE.Mesh(new THREE.SphereGeometry(1, 9) , new THREE.MeshBasicMaterial({color: 'yellow', wireframe: true}));
+  // mesh3 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'green'}));
+  // mesh4 = new THREE.Mesh(new THREE.BoxGeometry(1,1,1) , new THREE.MeshBasicMaterial({color: 'yellow'}));
+  // group = new THREE.Group();
 
   clock = new THREE.Clock();
   axisHelper = new THREE.AxesHelper(2);
@@ -22,47 +27,81 @@ export class AppComponent implements AfterViewInit, OnInit {
   counter: number = 0;
   time: number = 0;
   disabled: boolean = false;
-  countdown: number = 3;
+  countdown: number = this.COUNT_VDO_RENDER_IN_SEC;
 
   downloadBlob: Blob| null = null;
+  control: OrbitControls| undefined;
+  unsubscribe = new Subject<void>();
+  test: boolean = false;
   constructor() {}
   ngOnInit(): void {
-    this.mesh.position.x = 1.5;
-    this.mesh2.position.x = -1.5;
-    this.mesh3.position.y = 1.5;
-    this.mesh4.position.y = -1.5;
-    this.group.add(this.mesh, this.mesh2, this.mesh3, this.mesh4);
+    this.mesh.position.x = 2;
+    this.mesh.position.y = -2.5;
+    this.mesh2.position.x = -2;
+    this.mesh2.position.y = -2.5;
+    this.mesh3.position.x = 0;
+    this.mesh3.position.y = -2.5;
+    // this.mesh2.position.x = -1.5;
+    // this.mesh3.position.y = 1.5;
+    // this.mesh4.position.y = -1.5;
+    // this.group.add(this.mesh, this.mesh2, this.mesh3, this.mesh4);
     // this.group.rotation.x = Math.PI / 2;
     
     this.camera.position.z = 10;
     this.scene.add(this.camera);
-    this.scene.add(this.group);
-    this.scene.add(this.axisHelper);
+    // this.scene.add(this.group);
+    this.scene.add(this.mesh);
+    this.scene.add(this.mesh2);
+    this.scene.add(this.mesh3);
+    // this.scene.add(this.axisHelper);
+    fromEvent(window, 'resize').pipe(takeUntil(this.unsubscribe)).subscribe(() => {
+      this.sizes = { width: window.innerWidth, height: window.innerHeight };
+      this.renderer.setSize(this.sizes.width, this.sizes.height);
+    })
+  }
+  
+  swap() {
+    this.test = !this.test;
+    // this.renderer.render(this.scene, this.camera);
   }
 
   ngAfterViewInit(): void {
     const canvas = document.querySelector('.webgl') as HTMLCanvasElement;
+    this.control = new OrbitControls(this.camera, canvas);
+    this.control.enableDamping = true;    
     this.renderer = new THREE.WebGLRenderer({canvas});
     this.renderer.setSize(this.sizes.width, this.sizes.height);
     console.time('frame rate');
     this.time = Date.now();
-    this.tick();
+    // gsap.to(this.group.position, {duration: 2, delay: 1, x: 3});
+    // gsap.to(this.group.position, {duration: 2, delay: 1, x: -3});
+    const clock = new THREE.Clock();
+    this.tick(clock);
   }
   
 
-  tick() {
-    // const currentTime = Date.now();
-    // const deltaTime = currentTime - this.time;
-    // this.time = currentTime;
+  tick(clock: THREE.Clock) {
+    const currentTime = Date.now();
+    const deltaTime = currentTime - this.time;
+    this.time = currentTime;
+    this.mesh.rotation.y = clock.getElapsedTime();
+    this.mesh2.rotation.y = -clock.getElapsedTime();
     // this.group.rotation.z += deltaTime * 0.001;
-    this.group.rotation.z = this.clock.getElapsedTime();
+    // this.group.rotation.z = this.clock.getElapsedTime();
     // this.group.rotation.y = this.clock.getElapsedTime();
-    this.group.rotation.x = this.clock.getElapsedTime();
-    this.group.position.x = Math.sin(this.clock.getElapsedTime());
-    this.group.position.y = Math.cos(this.clock.getElapsedTime());
+    // this.group.rotation.x = this.clock.getElapsedTime();
+    // this.group.position.x = Math.sin(this.clock.getElapsedTime());
+    // this.group.position.y = Math.cos(this.clock.getElapsedTime());
+    if (this.test) {
+      this.mesh3.rotation.z = clock.getElapsedTime() / 4;
+      this.camera.lookAt(this.mesh.position);
+    } else {
+      this.mesh3.rotation.z = -clock.getElapsedTime() / 4;
+      this.camera.lookAt(this.mesh2.position);
+    }
     this.renderer.render(this.scene, this.camera);
     this.counter++;
-    window.requestAnimationFrame(() => this.tick());
+    window.requestAnimationFrame(() => this.tick(clock));
     if (this.counter === 60) {
       console.timeLog('frame rate');
       this.counter = 0;
@@ -91,11 +130,11 @@ export class AppComponent implements AfterViewInit, OnInit {
         rec.stop();
         clearInterval(countdownInterval);
         this.disabled = false;
-        this.countdown = 3;
+        this.countdown = this.COUNT_VDO_RENDER_IN_SEC;
       } else {
         this.countdown--;
       }
-    }, 1000); // stop recording in 3s
+    }, 1000);
   }
 
   download() {
